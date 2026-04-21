60df31887fba2a1cb4f903df16a94dc06fc204dd
ABDUL AZEEZ <azeezazeez7989@gmail.com>
2026-04-21 17:55:59 +0530

    AdminController.java

--- Cartify-backend/src/main/java/com/cartify/backend/controller/AdminController.java
+++ Cartify-backend/src/main/java/com/cartify/backend/controller/AdminController.java
@@ -15,7 +15,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/admin")
-@CrossOrigin(origins = "*")
+@CrossOrigin("*")
 @PreAuthorize("hasRole('ADMIN')")
 public class AdminController {
 
