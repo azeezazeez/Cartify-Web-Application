138425b97f1680f39e425f68513444710275e799
ABDUL AZEEZ <azeezazeez7989@gmail.com>
2026-04-21 23:48:51 +0530

    Update ProductModal.tsx

--- Cartify-frontend/src/components/ProductModal.tsx
+++ Cartify-frontend/src/components/ProductModal.tsx
@@ -68,12 +68,14 @@ export const ProductModal: React.FC<ProductModalProps> = ({
 
             <div className="flex-1 p-8 md:p-12 overflow-y-auto no-scrollbar">
               <div className="space-y-6">
-                <div className="mb-4 -ml-8 md:-ml-12">
-                  <p className="text-black font-bold text-sm">
+                 {/* Title FULL LEFT (no padding at all) */}
+              <div className="px-4 md:px-0 pt-6">
+              <p className="text-black font-bold text-sm">
                   Our Products
-              </p>
-                  </div>
-                <div>
+                      </p>
+                      </div>
+                  <div className="p-8 md:p-12 space-y-6">
+                   <div>
                   <p className="text-xs font-bold text-brand-500 dark:text-brand-400 uppercase tracking-[0.2em] mb-2">
                     {product.category}
                   </p>
