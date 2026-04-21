d6371624b1f3af6cf319d4afadca5d3af31db468
ABDUL AZEEZ <azeezazeez7989@gmail.com>
2026-04-21 18:35:02 +0530

    Update SecurityConfig.java

--- Cartify-backend/src/main/java/com/cartify/backend/config/SecurityConfig.java
+++ Cartify-backend/src/main/java/com/cartify/backend/config/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+               
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll()  // Let JwtFilter handle authentication
                 )
